9bf95c6e06f50e4183feae08c04c41984d5e4384
Charlie <charles.ae.dummett@outlook.com>
2022-04-27 13:51:31 +0100

    method '_scrape_table' added

--- src/modules/scraper.py
+++ src/modules/scraper.py
@@ -27,6 +27,39 @@ class FbRefScraper:
         self._log.setLevel(level=level)
         self._log.debug(msg="'__init__' method called.")
 
+    def _scrape_table(self, url: str, table_id: str):
+        """
+        Scrapes the specified table from the specified url.
+
+        Function makes a request to the specified url, coverts the html response into a BeautifulSoup object, parses
+        through each table in the soup until a match is found.
+
+        Args:
+            url:
+            table_id:
+
+        Returns:
+            A BeautifulSoup object containing the html data for the specified table.
+
+        Raises:
+            ValueError: If no table with an id matching table_id can be found.
+
+        """
+        self._log.debug("'scrape_table' method called.")
+
+        res = requests.get(url)
+        comm = re.compile("<!--|-->")
+        soup = BeautifulSoup(comm.sub("", res.text), 'lxml')
+        all_tables = soup.find_all('table')
+
+        for table in all_tables:
+            if 'id' not in table.attrs:
+                continue
+            if table['id'] == table_id:
+                return table
+
+        error_msg = f"Invalid argument 'table_id'. A table with id '{table_id}' was not found in any table tag."
+        raise ValueError(error_msg)
 
 if __name__ == "__main__":
     """"""
