5ed513fd4e940059a7aae68ddfec88838b96cdd0
Charlie <charles.ae.dummett@outlook.com>
2022-04-27 13:49:28 +0100

    class 'FbRefScraper' added

--- src/modules/scraper.py
+++ src/modules/scraper.py
@@ -8,3 +8,28 @@ import requests
 import pandas as pd
 
 from bs4 import BeautifulSoup
+
+
+class FbRefScraper:
+    """"""
+
+    def __init__(self, level=logging.WARNING):
+        """
+        Creates an instance of the FbRefScraper class.
+
+        Function initialises an instance of the class by creating a logger matching the class name and setting the log
+        level at the specified level.
+
+        Args:
+            level:
+        """
+        self._log = logging.getLogger("FbRefScraper")
+        self._log.setLevel(level=level)
+        self._log.debug(msg="'__init__' method called.")
+
+
+if __name__ == "__main__":
+    """"""
+    logging.basicConfig(level=logging.WARNING)
+    scraper = FbRefScraper(level=logging.DEBUG)
+
