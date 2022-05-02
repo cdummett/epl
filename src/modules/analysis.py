6b5cb980b40a5b886151ca564cda57ad4ffae39b
cdummett <charles.ae.dummett@outlook.com>
2022-05-02 09:15:09 +0100

    module 'analysis' updated
    Updates reflect changes to the 'scraper' module in the previous commit and make refactors code.

--- src/modules/analysis.py
+++ src/modules/analysis.py
@@ -36,12 +36,21 @@ class SquadAnalysisGui:
 
         # Initialise widgets
         self.root = tk.Tk()
-        self.frame_x_data_controls = DataControlFrame(level=level, master=self.root, relief='groove', borderwidth=2)
-        self.frame_x_data_controls.grid(row=0, column=0, padx=self.PAD_X, pady=self.PAD_Y)
-        self.frame_y_data_controls = DataControlFrame(level=level, master=self.root, relief='groove', borderwidth=2)
-        self.frame_y_data_controls.grid(row=1, column=0, padx=self.PAD_X, pady=self.PAD_Y)
-        self.button_update = tk.Button(master=self.root, text="Update", width=40, command=self.update, relief='groove')
-        self.button_update.grid(row=2, column=0, padx=self.PAD_X, pady=self.PAD_Y)
+        self.x_data_control_frame = DataControlFrame(level=level,
+                                                     master=self.root,
+                                                     relief='groove',
+                                                     borderwidth=2,
+                                                     callback=self.update)
+
+        self.y_data_control_frame = DataControlFrame(level=level,
+                                                     master=self.root,
+                                                     relief='groove',
+                                                     borderwidth=2,
+                                                     callback=self.update)
+
+        # Pack data control frame widgets
+        self.x_data_control_frame.grid(row=0, column=0, padx=self.PAD_X, pady=self.PAD_Y)
+        self.y_data_control_frame.grid(row=1, column=0, padx=self.PAD_X, pady=self.PAD_Y)
 
         # Initialise analysis figure
         self.fig = plt.figure(num=1)
@@ -51,11 +60,11 @@ class SquadAnalysisGui:
         # Run app
         self.root.mainloop()
 
-    def update(self):
+    def update(self, *args):
         """"""
         self._log.debug(msg="'update' method called.")
-        x = self.frame_x_data_controls.df[self.frame_x_data_controls.variable_metric.get()]
-        y = self.frame_y_data_controls.df[self.frame_y_data_controls.variable_metric.get()]
+        x = self.x_data_control_frame.df[self.x_data_control_frame.metric_menu.variable.get()]
+        y = self.y_data_control_frame.df[self.y_data_control_frame.metric_menu.variable.get()]
 
         df = pd.merge(x, y, left_index=True, right_index=True)
 
@@ -70,7 +79,7 @@ class DataControlFrame(tk.Frame):
     PAD_X = 1
     PAD_Y = 1
 
-    def __init__(self, level=logging.WARNING, **kw):
+    def __init__(self, level=logging.WARNING, callback=None, **kw):
         """
         Creates an instance of the DataControlFrame class.
 
@@ -95,28 +104,27 @@ class DataControlFrame(tk.Frame):
 
         # Create a scraper object for the data control frame and initialise the data
         self.scraper = FbRefScraper(level=logging.WARNING)
-        self.df = self.scraper.scrape_squad_summaries(stat='stats', vs=False)
+        self.df = self.scraper.scrape_squad_summaries(stat='stats', vs='for')
 
-        # Initialise the values for the OptionMenu widgets
-        self.stat_options = list(self.scraper.SUMMARY_STAT_OPTS.keys())
-        self.metric_options = list(self.df.columns)
+        #
+        self.stat_menu = MenuFrame(master=self,
+                                   level=level,
+                                   values=list(self.scraper.SUMMARY_STAT_OPTS.keys()),
+                                   callback=self.update_metric_menu)
+        self.vs_menu = MenuFrame(master=self,
+                                 level=level,
+                                 values=['for', 'against'],
+                                 callback=self.update_metric_menu)
+        self.metric_menu = MenuFrame(master=self,
+                                     level=level,
+                                     values=list(self.df.columns),
+                                     callback=callback)
 
-        # Initialise the variables for the OptionMenu widgets
-        self.variable_stat = tk.StringVar()
-        self.variable_stat.set(self.stat_options[0])
-        self.variable_stat.trace(mode="w", callback=self.callback_optionmenu_stat)
-        self.variable_metric = tk.StringVar()
-        self.variable_metric.set(self.metric_options[0])
+        self.stat_menu.grid(row=0, column=0, padx=self.PAD_X, pady=self.PAD_Y)
+        self.vs_menu.grid(row=1, column=0, padx=self.PAD_X, pady=self.PAD_Y)
+        self.metric_menu.grid(row=2, column=0, padx=self.PAD_X, pady=self.PAD_Y)
 
-        # Initialise the OptionMenu widgets
-        self.optionmenu_stat = tk.OptionMenu(self, self.variable_stat, *self.stat_options)
-        self.optionmenu_stat.config(width=40)
-        self.optionmenu_stat.grid(row=0, column=0, padx=self.PAD_X, pady=self.PAD_Y)
-        self.optionmenu_metric = tk.OptionMenu(self, self.variable_metric, *self.metric_options)
-        self.optionmenu_metric.config(width=40)
-        self.optionmenu_metric.grid(row=1, column=0, padx=self.PAD_X, pady=self.PAD_Y)
-
-    def callback_optionmenu_stat(self, *args):
+    def update_metric_menu(self, *args):
         """
         Updates values in the metric OptionMenu widget when the selection in the stat OptionMenu widget is changed.
 
@@ -130,23 +138,19 @@ class DataControlFrame(tk.Frame):
             None
 
         """
-
         # Function logging message
         self._log.debug(msg="'callback_optionmenu_stat' method called.")
 
-        self.df = self.scraper.scrape_squad_summaries(stat=self.variable_stat.get(), vs=False)
-        self.metric_options = list(self.df.columns)
-        self.variable_metric.set(self.metric_options[0])
+        stat = self.stat_menu.variable.get()
+        vs = self.vs_menu.variable.get()
 
-        self.optionmenu_metric["menu"].delete(0, "end")
-        for string in self.metric_options:
-            self.optionmenu_metric["menu"].add_command(label=string,
-                                                       command=lambda value=string: self.variable_metric.set(value))
+        self.df = self.scraper.scrape_squad_summaries(stat=stat, vs=vs)
+        self.metric_menu.update_values(values=list(self.df.columns))
 
 
 class MenuFrame(tk.Frame):
 
-    def __init__(self, level=logging.WARNING, values=None, **kw):
+    def __init__(self, level=logging.WARNING, values=None, callback=None, **kw):
         """
         Creates an instance of the DataControlFrame class.
 
@@ -175,6 +179,8 @@ class MenuFrame(tk.Frame):
 
         self.variable = tk.StringVar()
         self.variable.set(values[0])
+        if callback:
+            self.variable.trace(mode="w", callback=callback)
 
         self.button_dw = tk.Button(master=self, text='<-', command=self._button_dw)
         self.menu = tk.OptionMenu(self, self.variable, *values)
